6786f0aa2c0e2ebd53aebc5c6ec7bcabd2403aaf
Guilherme Sampaio <guilherme.sampaio324@hotmail.com>
2025-01-20 22:42:20 -0300

    Criando conexoes MySQL

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -50,5 +50,8 @@
         <footer>
             <p>Guilherme &copy; 2025</p>
         </footer>
+
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     </body>
 </html>
