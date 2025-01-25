499225e4d4765654173621998c926c2164d87f77
Guilherme Sampaio <guilherme.sampaio324@hotmail.com>
2025-01-25 19:23:23 -0300

    Show Event

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,6 +4,53 @@
 
 @section('content')
 
-<h1>Criar um evento</h1>
+<div id="event-create-container" class="col-md-6 offset-md-3">
+    <h1>Crie o seu evento</h1>
+    <form action="/events" method="POST" style="display:flex; gap: 20px; flex-direction: column;" enctype="multipart/form-data">
+        @csrf
+        <div class="form-group">
+            <label for="title">Imagem do Evento</label>
+            <input type="file" class="form-control-file" id="image" name="image">
+        </div>
+        <div class="form-group">
+            <label for="title">Evento:</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+        </div>
+        <div class="form-group">
+            <label for="title">Cidade:</label>
+            <input type="text" class="form-control" id="city" name="city" placeholder="Nome do evento">
+        </div>
+        <div class="form-group">
+            <label for="title">O evento é privado?</label>
+            <select name="private" id="private" class="form-control">
+                <option value="0">Não</option>
+                <option value="1">Sim</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="title">Descrição:</label>
+            <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="title">Adicione itens de infraestrutura:</label>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Cadeiras"> Cadeiras
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Palco"> Palco
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Bebida grátis"> Bebida grátis
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Open Food"> Open Food
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Brindes"> Brindes
+            </div>
+        </div>
+        <input type="submit" class="btn btn-primary" value="Criar Evento" style="max-width: 120px;">
+    </form>
+</div>
 
 @endsection
