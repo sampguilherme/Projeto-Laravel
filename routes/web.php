2138ccedae67a47475ae954ceba6763df2102443
Guilherme Sampaio <guilherme.sampaio324@hotmail.com>
2025-01-27 22:15:19 -0300

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,4 @@ Route::get('/events/create', [EventController::class, 'create'])->middleware('au
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::post('/events', [EventController::class, 'store']);
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
